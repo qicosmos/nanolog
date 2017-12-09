a39aae9e89718f763aa15ba82e375539519789f9
Gogs <gogs@fake.local>
2017-12-09 16:37:32 +0800

    add

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include "nanolog.hpp"
+
+int main() {
+	nanolog::initialize(nanolog::GuaranteedLogger(), "/tmp/", "nanolog", 1);
+
+	// Or if you want to use the non guaranteed logger -
+	// ring_buffer_size_mb - LogLines are pushed into a mpsc ring buffer whose size
+	// is determined by this parameter. Since each LogLine is 256 bytes,
+	// ring_buffer_size = ring_buffer_size_mb * 1024 * 1024 / 256
+	// In this example ring_buffer_size_mb = 3.
+	// nanolog::initialize(nanolog::NonGuaranteedLogger(3), "/tmp/", "nanolog", 1);
+
+	for (int i = 0; i < 5; ++i)
+	{
+		LOG_INFO << "Sample NanoLog: " << i;
+	}
+
+	return 0;
+}
